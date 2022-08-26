8b6fcb854222ef43d4a2a7c9342d633698ddabcd
Aminadabes <aminadabes.filho@cidadania.gov.br>
2022-08-26 09:28:06 -0300

    Firt Commit

---
+++ src/test/java/com/github/aminadabes/comerfood/comerfood/ComerFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aminadabes.comerfood.comerfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComerFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
